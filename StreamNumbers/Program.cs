94938586bff5a65e76dbe91dd0c7877903accb61
Leon <Leon.ID@QQ.COM>
2020-04-10 14:15:48 +0800

    Modified;

--- StreamNumbers/Program.cs
+++ StreamNumbers/Program.cs
@@ -5,6 +5,10 @@ namespace StreamNUmbers
 {
     class Program
     {
+        /* 扩展题：
+         * 说出一个 3*4 的矩阵，从左下角走到右下角的最短路径共有几种路径方案 => 10
+         * x*y的矩阵，最短路径总数 => Cx * Cy，x和y的组合的乘积
+         */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
