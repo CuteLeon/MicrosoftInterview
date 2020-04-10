6f8b352e147c51a43ef4fd823a4d8281addbf198
Leon <Leon.ID@QQ.COM>
2020-04-10 11:00:26 +0800

    流式数组求平均值

---
+++ StreamNumbers/MovingAverage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StreamNumbers
+{
+    public class MovingAverage
+    {
+        private readonly int windowSize;
+        private int lastIndex = 0;
+        private int count = 0;
+        private int currentSum = 0;
+        private readonly int[] window;
+
+        public MovingAverage(int windowSize)
+        {
+            if (windowSize <= 0)
+                throw new ArgumentException(nameof(windowSize));
+
+            this.windowSize = windowSize;
+            this.window = new int[windowSize];
+        }
+
+        public double Next(int nextNumber)
+        {
+            this.currentSum += nextNumber;
+            if (this.count < this.windowSize)
+            {
+                this.window[this.lastIndex++] = nextNumber;
+                this.count++;
+            }
+            else if (this.count >= this.windowSize)
+            {
+                this.currentSum -= this.window[lastIndex];
+                this.window[this.lastIndex++] = nextNumber;
+            }
+            if (this.lastIndex == this.windowSize)
+            {
+                this.lastIndex = 0;
+            }
+
+            return this.currentSum / (double)count;
+        }
+    }
+}
---
+++ StreamNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using StreamNumbers;
+
+namespace StreamNUmbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var window_0 = new MovingAverage(3);
+            // 1 
+            // 3
+            // 5
+            // 200
+        }
+    }
+}
